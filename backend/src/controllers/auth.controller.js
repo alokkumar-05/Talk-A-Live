674d92b6289a11ba3f2c42edf1d4e3434fd12670
Alok <alokkumarswn01@gmail.com>
2026-02-01 11:51:45 +0530

    Database Setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+export const signup =(req,res)=>{
+  const {fullname , email , password} = req.body
+  try{
+    //hash password
+    
+  }catch(error){
+
+  }
+};
+
+export const login =(req,res)=>{
+  res.send("login route");
+};
+
+export const logout =(req,res)=>{
+  res.send("logout route");
+};
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,23 @@
 import express from "express";
-
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv"; 
+import { connectDB } from "./lib/db.js";
 
+dotenv.config()
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Talk-A-Live Backend is running 🚀");
+});
 
 app.use("/api/auth",authRoutes)
 
-app.listen(5001,()=>{
-  console.log("server is running on port 5001")
-})
+const PORT = process.env.PORT || 5001 ;
+
+
+app.listen(PORT,()=>{
+  console.log("server is running on Port:"+ PORT);
+  connectDB();
+});
