96ca2fd0fac84afe792552939dfaa82164dee118
Alok <alokkumarswn01@gmail.com>
2026-02-04 17:10:00 +0530

    Profile page added

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,8 @@ import cors from "cors";
 dotenv.config()
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "3mb" }));
+app.use(express.urlencoded({ limit: "3mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
   origin:"http://localhost:5173",
