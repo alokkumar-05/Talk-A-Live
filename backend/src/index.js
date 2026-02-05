3d4ba82b5f687ed7bc05054dc97272c6f20d0980
Alok <alokkumarswn01@gmail.com>
2026-02-05 14:35:55 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,11 @@ import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import {app,server} from "./lib/socket.js";
-
-
+import path from "path";
 dotenv.config();
+
 const PORT = process.env.PORT || 5001 ;
+const __dirname = path.resolve();
 
 
 app.use(express.json({ limit: "3mb" }));
@@ -29,6 +30,13 @@ app.get("/", (req, res) => {
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT,()=>{
   console.log("server is running on Port:"+ PORT);
